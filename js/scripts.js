dd2b646ddaf7cf8ba0559660398122bad3348cab
Baul Kim <baul.kim43@gmail.com>
2019-11-01 15:17:03 -0700

    add correct if statement for second spec.

--- js/scripts.js
+++ js/scripts.js
@@ -3,17 +3,18 @@ var countBack = function(numberInputed) {
 
 // var firstException = ["1"]
 // var secondException = ["2"]
+var thirdException = [3, 13, 23]
 // var exceptionOne = ["Beep!"]
 // var exceptionTwo = ["Boop!"]
 var exceptionThree = ["I'm sorry, Dave. I'm afraid I can't do that."]
 var newArray = [];
 for (i = 0; i <= numberInputed; newArray.push(i++ + " ")) {
+  if (thirdException.includes(numberInputed)) {
+    
+  }
 }
   numberInputed = newArray;
-  var thirdException = ["3", "13", "23"]
-    if (numberInputed.includes(thirdException)) {
-      alert("hi!");
-    }
+
 
   return numberInputed;
 

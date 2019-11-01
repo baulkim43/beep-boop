c761721a2110011d97127d060f9c0b3aa40b4831
Baul Kim <baul.kim43@gmail.com>
2019-11-01 16:27:35 -0700

    remove not working second spec's code. try regular expressions.

--- js/scripts.js
+++ js/scripts.js
@@ -3,18 +3,20 @@ var countBack = function(numberInputed) {
 
 // var firstException = ["1"]
 // var secondException = ["2"]
-var thirdException = /"3"/gi;
+var thirdException = ["3"]
 // var exceptionOne = ["Beep!"]
 // var exceptionTwo = ["Boop!"]
-var exceptionThree = ["I'm sorry, Dave. I'm afraid I can't do that."]
+// var exceptionThree = ["I'm sorry, Dave. I'm afraid I can't do that."]
 var newArray = [];
 for (i = 0; i <= numberInputed; newArray.push(i++ + " ")) {
+  // if (numberInputed.includes(thirdException)) {
 
+  
 
 }
+numberInputed = newArray;
+console.log(newArray);
 
-  numberInputed = newArray;
-  console.log(numberInputed);
 
 
   return numberInputed;

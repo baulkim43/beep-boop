587a2ca286759fd0c1a7f77aa9f1d041174887f0
Baul Kim <baul.kim43@gmail.com>
2019-11-01 16:00:42 -0700

    still work on 2nd spec.

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,14 @@ var countBack = function(numberInputed) {
 
 // var firstException = ["1"]
 // var secondException = ["2"]
-var thirdException = [3]
+var thirdException = /"3"/gi;
 // var exceptionOne = ["Beep!"]
 // var exceptionTwo = ["Boop!"]
 var exceptionThree = ["I'm sorry, Dave. I'm afraid I can't do that."]
 var newArray = [];
 for (i = 0; i <= numberInputed; newArray.push(i++ + " ")) {
 
-  
+
 }
 
   numberInputed = newArray;

07d0d4ca1ef614c083eaef1b5f6875cdb4f3bf07
Baul Kim <baul.kim43@gmail.com>
2019-11-01 14:26:32 -0700

    still work on second spec.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,25 @@
 // business logic
 var countBack = function(numberInputed) {
 
+// var firstException = ["1"]
+// var secondException = ["2"]
+var thirdException = ["3", "13", "23"]
+// var exceptionOne = ["Beep!"]
+// var exceptionTwo = ["Boop!"]
+var exceptionThree = ["I'm sorry, Dave. I'm afraid I can't do that."]
 var newArray = [];
 for (i = 0; i <= numberInputed; newArray.push(i++ + " ")) {
 }
 numberInputed = newArray;
+
+if (thirdException.includes(numberInputed)) {
+  numberInputed = (numberInputed.slice(numberInputed.indexOf(thirdException))).replace(exceptionThree);
+  console.log(numberInputed);
+}
 return numberInputed;
 
+
 }
-
-
-
-
-
-
 
 // user interface logic
 $(document).ready(function() {
